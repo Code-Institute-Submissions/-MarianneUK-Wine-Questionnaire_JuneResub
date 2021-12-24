2cd884b877f2ee62226fdf91caef77fc3f176d0c
Marianne J <marianne.johany@gmail.com>
2021-12-24 15:08:34 +0000

    Add first variables to js file.

---
+++ script.js
@@ -0,0 +1,12 @@
+//initialise the scores for each type
+let type1Score = 0;
+let type2Score = 0;
+let type3Score = 0;
+let type4Score = 0;
+// other variables
+let scores = [];
+const grocery = document.getElementById('grocery');
+const art = document.getElementById('art');
+const geek = document.getElementById('geek');
+const snob = document.getElementById('snob');
+const endResult = document.getElementById('result');
